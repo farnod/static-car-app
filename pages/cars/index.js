c1ab3df3e6bd5afb8d1b4369ec25f51a533feea1
mohamad farnoud <farnoodcc@gmail.com>
2023-02-04 16:23:29 +0330

    car page add

---
+++ pages/cars/index.js
@@ -0,0 +1,25 @@
+// import CarsPage from "../../components/templates/carsPage"
+// import carsData from "../../data/carsData"
+
+// function Details() {
+//   return (
+//     <div>
+//       <CarsPage data={carsData} />
+//     </div>
+//   )
+// }
+
+// export default Details
+
+import CarsPage from "../../components/templates/CarsPage";
+import carsData from "../../data/carsData";
+
+function Details() {
+  return (
+    <div>
+      <CarsPage data={carsData} />
+    </div>
+  );
+}
+
+export default Details;
