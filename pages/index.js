fdc016ec83b08d36b267369b7471c21defa71d73
mohamad farnoud <farnoodcc@gmail.com>
2023-02-08 09:25:54 +0330

    allbutton component added

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,13 @@
 import carsData from "@/data/carsData";
 import CarsPage from "@/components/templates/CarsPage";
 import Categories from "@/components/module/Categories";
+import AllButton from "@/components/module/AllButton";
 
 function Index() {
   const cars = carsData.slice(0, 3);
 	return (
 		<div>
+      <AllButton />
       <Categories />
 			<CarsPage data={cars} />
 		</div>
